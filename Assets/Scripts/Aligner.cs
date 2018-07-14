175bdf402761d1a249f2e353485d007c40080e1c
Majak <majakthecoder@gmail.com>
2018-07-13 22:50:59 -0700

    wip

--- Assets/Scripts/Aligner.cs
+++ Assets/Scripts/Aligner.cs
@@ -26,9 +26,32 @@ public class Aligner : MonoBehaviour
         if (count < 3)
             return;
 
-        int aa = "aaa".CompareTo("bbb");
-        int ba = "aaa".CompareTo("aaa");
-        int ca = "bbb".CompareTo("aaa");
+        var selected = new GameObject[count];
+        System.Array.Copy(Selection.gameObjects, selected, count);
+
+        System.Array.Sort(selected, (a, b) => { return a.name.CompareTo(b.name); });
+
+        var startPosition = selected[0].transform.position;
+        var endPosition = selected[count - 1].transform.position;
+
+        var direction = endPosition - startPosition;
+        var length = direction.magnitude;
+        float step = length / (count - 1);
+        direction.Normalize();
+
+        for (int i = 0; i < count; i++)
+        {
+            selected[i].transform.position = startPosition + direction * step * i;
+        }
+    }
+
+    [MenuItem("Align/Rename")]
+    public static void Rename()
+    {
+        var count = Selection.gameObjects.Length;
+
+        if (count == 0)
+            return;
 
         var selected = new GameObject[count];
         System.Array.Copy(Selection.gameObjects, selected, count);
@@ -43,12 +66,9 @@ public class Aligner : MonoBehaviour
         float step = length / (count - 1);
         direction.Normalize();
 
-        Debug.LogFormat("first: {0}, end: {1}", selected[0].name, selected[count - 1].name);
-
-
         for (int i = 0; i < count; i++)
         {
-            selected[i].transform.position = startPosition + direction * step * i;
+            selected[i].name = string.Format("point_{0:D2}", i);
         }
     }
 }
