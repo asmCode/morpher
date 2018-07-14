8f641cf6cefe3996cde3487c15f5908d410ce900
Majak <majakthecoder@gmail.com>
2018-07-14 00:10:47 -0700

    wip

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    public ShapeRenderer m_shapeRenderer;
+    public Transform m_shape;
+
+    void Start()
+    {
+        Vector3[] points = new Vector3[m_shape.childCount];
+        for (int i = 0; i < m_shape.childCount; i++)
+            points[i] = m_shape.GetChild(i).transform.position;
+
+        m_shapeRenderer.DrawShape(points);
+    }
+}
---
+++ Assets/Scripts/ShapeRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeRenderer : MonoBehaviour
+{
+    public GameObject m_linePrefab;
+    public Canvas m_canvas;
+
+    private void Awake()
+    {
+        for (int i = 0; i < 60; i++)
+        {
+            Instantiate(m_linePrefab, transform);
+        }
+    }
+
+    public void DrawShape(Vector3[] points)
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            var child = transform.GetChild(i);
+
+            Vector3 direction = points[(i + 1) % points.Length] - points[i];
+
+            child.localScale = new Vector3(1.0f / m_canvas.scaleFactor, direction.magnitude / m_canvas.scaleFactor, 1.0f);
+            child.up = direction;
+            child.position = points[i];
+        }
+    }
+}
