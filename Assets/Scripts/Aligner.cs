ed0e9c5311e1a8e4d424381c9a1286eb9db98f15
Majak <majakthecoder@gmail.com>
2018-07-13 21:30:30 -0700

    wip

---
+++ Assets/Scripts/Aligner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Aligner : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    [MenuItem("Align/Between")]
+    public static void AlignBetween()
+    {
+        var count = Selection.gameObjects.Length;
+
+        if (count < 3)
+            return;
+
+        int aa = "aaa".CompareTo("bbb");
+        int ba = "aaa".CompareTo("aaa");
+        int ca = "bbb".CompareTo("aaa");
+
+        var selected = new GameObject[count];
+        System.Array.Copy(Selection.gameObjects, selected, count);
+
+        System.Array.Sort(selected, (a, b) => { return a.name.CompareTo(b.name); });
+
+        var startPosition = selected[0].transform.position;
+        var endPosition = selected[count - 1].transform.position;
+
+        var direction = endPosition - startPosition;
+        var length = direction.magnitude;
+        float step = length / (count - 1);
+        direction.Normalize();
+
+        Debug.LogFormat("first: {0}, end: {1}", selected[0].name, selected[count - 1].name);
+
+
+        for (int i = 0; i < count; i++)
+        {
+            selected[i].transform.position = startPosition + direction * step * i;
+        }
+    }
+}
---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape
+{
+    public List<Vector3> m_points = new List<Vector3>();
+
+
+}
